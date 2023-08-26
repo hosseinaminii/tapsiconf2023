235c030c3747874be2e0e7554f8ca822e2282e4b
Amini <hosseinamiinii@gmail.com>
2023-08-26 23:46:18 +0330

    Reconfigure httpClient

--- shared/src/commonMain/kotlin/com/example/tapsiconf/GithubRepository.kt
+++ shared/src/commonMain/kotlin/com/example/tapsiconf/GithubRepository.kt
@@ -1,12 +1,20 @@
 package com.example.tapsiconf
 
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
 class GithubRepository {
     private val httpClient = httpClient {
         expectSuccess = true // To throw exception in case of error occurrence
         defaultRequest {
             url("https://api.github.com") // Base URL for all requests
+        }
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+            })
         }
     }
 }
